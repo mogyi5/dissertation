9d14bc90ea761476a4ea420703949ac9a855ed40
mogyi5 <szaboki.reka@gmail.com>
2019-07-09 15:59:11 +0100

    after first page

--- myhealthdb/views.py
+++ myhealthdb/views.py
@@ -9,12 +9,6 @@ def index(request):
     response = render(request, 'myhealthdb/index.html', context_dict)
 
     return response
-
-# def detail(request, question_id):
-#     return HttpResponse("You're looking at question %s." % question_id)
-
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-#     return HttpResponse(response % question_id)
+    
 
 
